5f70e51c2b78b4f15bbcb8615702204fc4f94739
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-11-29 08:13:59 +0000

    link First_layer to page Home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,10 @@
-
+import FirstLayer from '../compoments/moved/home/second_layer/second_layer'
 
 function PageHome() {
   return (
     <div className='PageHome'>
         {/* header */}
-
+        <FirstLayer />
         {/* First */}
         {/* second */}
         {/* footer */}
