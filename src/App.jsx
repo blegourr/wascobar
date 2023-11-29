fa3e3c423d8b081af7196330e18d4204a36aa48d
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-11-29 16:22:50 +0100

    add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 // import css
@@ -9,10 +8,14 @@ import Home from './page/Home'
 import Anglais from './page/Project/Anglais'
 import NotFound from './page/NotFound';
 
+// import fixed element
+import HeaderCompongnement from './compoments/fixed/header/header';
+
 
   function App() {
     return (
       <BrowserRouter>
+        <HeaderCompongnement/>
         <Routes>
           <Route
             path='/'
