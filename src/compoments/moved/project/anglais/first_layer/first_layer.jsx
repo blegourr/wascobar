f84284bc4de15c77c80868148096ba4b06536ec5
Khalesis <126458936+Khalesis@users.noreply.github.com>
2023-11-29 08:47:27 +0000

    first_layer update + css

--- src/compoments/moved/project/anglais/first_layer/first_layer.jsx
+++ src/compoments/moved/project/anglais/first_layer/first_layer.jsx
@@ -1,14 +1,21 @@
+// import css
+import './firstLayer.css'
+
+
+// import img
 import baba from './assets/baptiste.jpg.jpg'
 
 function first_layer_project_anglais() {
   return (
     <div className='first_layer_project_anglais'>
-      <div className="titre">
-        <h1>Proposé par Baptiste</h1>
-      </div>
-      <div className="desc">
-        <img src={baba} alt="" />
-        <p>blablablablablablablablab</p>
+      <div className='container'>
+        <div className="titre">
+          <h1>Proposé par Baptiste</h1>
+        </div>
+        <div className="desc">
+          <img src={baba} alt="" />
+          <p>blablablablablabla blalablablablablablablablab lablablablzab lazblablablablablablabla blablablablablabl alablabla blablablablablab lablablablzablazblablab lablablablablablablab lablablablalablablabla blablabl ablablablab lablzablazblablablablablabl ablablab lablablablablalablablabla blablablab lablablablabl zablazblab labl ablabl</p>
+        </div>
       </div>
     </div>
   );
---
+++ src/compoments/moved/project/anglais/second_layer/second_layer.jsx
@@ -0,0 +1,12 @@
+
+
+function second_layer() {
+    return (
+      <div className='second_layer'>
+        
+      </div>
+    );
+  }
+  
+  
+  export default second_layer;
