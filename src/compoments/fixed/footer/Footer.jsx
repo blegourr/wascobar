d61e613ee9d4862fd98511ead8a77f63d839b305
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-11-29 17:11:47 +0100

    preparation for deploy to vercel

---
+++ src/compoments/fixed/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import './header.css'
+
+
+// import d'image
+import Logo from './assets/logo.jpg'
+
+function HeaderCompongnement() {
+  const nav = useNavigate();
+
+  return (
+    <div className='HeaderCompongnement'>
+     
+    </div>
+  );
+}
+
+
+export default HeaderCompongnement;
