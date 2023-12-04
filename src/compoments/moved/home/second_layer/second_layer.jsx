e93cd0f2996b8acaa5844c57492829195827e09f
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-12-04 09:55:43 +0000

    refonte css part Math

--- src/compoments/moved/home/second_layer/second_layer.jsx
+++ src/compoments/moved/home/second_layer/second_layer.jsx
@@ -4,22 +4,22 @@ import './second_layer.css'
 function home_second_layer() {
   return (
     <div className='home_second_layer'>
-      <h1>Nos Projets</h1>
+      <h1 className='title'>Nos Projets</h1>
       <div className="containerCard">
         <div className="card">
           <h1>Dualité</h1>
-          <img src={Anglais} alt="" />
-          <p>Blablablabbalbalbalbalbalabaalblablablbalbalbabla</p>
+          <img src={Romance} alt="" />
+          <p>Blablab labbalbalbalb albala baal blablabl balb albabla</p>
         </div>
         <div className="card">
           <h1>Anglais</h1>
           <img src={Anglais} alt="" />
-          <p>blablablablablalbalblalbalabblablablabla</p>
+          <p>blab lablablabl albal blalbalabblab lab labla</p>
         </div>
         <div className="card">
           <h1>Romance</h1>
           <img src={Romance} alt="" />
-          <p>Blablablablablabalablabalbalabla</p>
+          <p>Blab lablablab labalabla balbala bla</p>
         </div>
       </div>
     </div>
