98186bc4f9f80171964dc7b01177635526129d26
Khalesis <126458936+Khalesis@users.noreply.github.com>
2023-12-04 10:49:32 +0000

    second layer

--- src/page/Project/Anglais.jsx
+++ src/page/Project/Anglais.jsx
@@ -1,9 +1,12 @@
 import FirstLayer from '../../compoments/moved/project/anglais/first_layer/first_layer'
+import SecondLayer from '../../compoments/moved/project/anglais/second_layer/second_layer';
 
 function PageAnglais() {
     return (
       <div className='PageAnglais'>
         <FirstLayer />
+        <SecondLayer />
+
       </div>
     );
   }
