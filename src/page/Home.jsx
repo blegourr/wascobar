3ff203ddb6ac864048d5506284b28db7572b90ad
Khalesis <126458936+Khalesis@users.noreply.github.com>
2023-12-06 08:31:45 +0000

    root design 404, header et footer sur chaque page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,18 @@
+import HeaderCompongnement from '../compoments/fixed/header/header';
 import FirstLayer from '../compoments/moved/home/second_layer/second_layer'
+import FooterCompongnement from '../compoments/fixed/footer/Footer'
 
 function PageHome() {
   return (
     <div className='PageHome'>
+        <HeaderCompongnement/>
         {/* header */}
         <FirstLayer />
         {/* First */}
         {/* second */}
         {/* footer */}
+        <FooterCompongnement />
+
     </div>
   );
 }
