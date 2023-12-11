6227ce6837c608b261f37a8cf67a5d5a9be836ca
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-12-11 18:11:48 +0000

    start Easter_Egg page

--- src/compoments/moved/EasterEgg/first_layer/First_layers.jsx
+++ src/compoments/moved/EasterEgg/first_layer/First_layers.jsx
@@ -4,9 +4,42 @@ import './First_layers.css'
 function FirstLayerEasterEgg() {
 
 
+
+
+
+
   return (
     <div className="first_layer_easterEgg">
-      oki
+      <h1>Easter Egg</h1>
+      <div className="containerCards">
+        <div className="card">
+          <div className="content-card">
+            <div className="background"></div>
+            <div className="content">
+              <h1>Easter_Egg_Page</h1>
+              <p>Vous avez trouver la page d'Easter_Egg</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="content-card">
+            <div className="background"></div>
+            <div className="content">
+              <h1>Easter_Egg_Page</h1>
+              <p>Vous avez trouver la page d'Easter_Egg</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="content-card">
+            <div className="background"></div>
+            <div className="content">
+              <h1>Easter_Egg_Page</h1>
+              <p>Vous avez trouver la page d'Easter_Egg</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
