9eac90c65052b919fce9cae7439ffd0c33a11b93
Math-222 <145916791+Math-222@users.noreply.github.com>
2023-12-13 08:51:34 +0000

    first_layer_contact

--- src/App.jsx
+++ src/App.jsx
@@ -11,47 +11,55 @@ import NotFound from './page/NotFound';
 import MentionsLegals from './page/MentionsLegals';
 import EasterEgg from './page/EasterEgg';
 import Soutiens from './page/Soutiens';
+import Contact from './page/Contact';
 
-  function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path='/'
-            exact
-            element={
-              <Home />
-            }
-          />
-          <Route
-            path='/Projets/Anglais'
-            exact
-            element={
-              <Anglais />
-            }
-          />
-            <Route
-            path='/Projets/Dualite'
-            exact
-            element={
-              <Dualite />
-            }
-          />
-            <Route
-            path='/Easter_egg'
-            exact
-            element={
-              <EasterEgg />
-            }
-          />
-           <Route
-            path='/Soutiens'
-            exact
-            element={
-              <Soutiens />
-            }
-          />
-          <Route
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          exact
+          element={
+            <Home />
+          }
+        />
+        <Route
+          path='/Projets/Anglais'
+          exact
+          element={
+            <Anglais />
+          }
+        />
+        <Route
+          path='/Projets/Dualite'
+          exact
+          element={
+            <Dualite />
+          }
+        />
+        <Route
+          path='/Easter_egg'
+          exact
+          element={
+            <EasterEgg />
+          }
+        />
+        <Route
+          path='/Soutiens'
+          exact
+          element={
+            <Soutiens />
+          }
+        />
+        <Route
+          path='/Contact'
+          exact
+          element={
+            <Contact />
+          }
+        />
+        <Route
           path='/Mentions_legal'
           exact
           element={
@@ -60,10 +68,10 @@ import Soutiens from './page/Soutiens';
         />
 
 
-           <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
