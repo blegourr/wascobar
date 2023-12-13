2e8ed5d45c3400f6202bff840655860ded254bcd
Khalesis <126458936+Khalesis@users.noreply.github.com>
2023-12-13 08:30:26 +0000

    Modifs responsive anglais et dualitée

--- src/compoments/moved/project/anglais/second_layer/second_layer.jsx
+++ src/compoments/moved/project/anglais/second_layer/second_layer.jsx
@@ -10,8 +10,8 @@ function second_layer() {
         <img src={baba2} alt="" />
         <p>Dans ce court métrage, on suit l'histoire d'un personnage lambda, et sa chute sans fin vers une folie qui prendra le dessus de son esprit, en mettant l'accent sur la personnification de cette folie,</p>
       </div>
-      <div className="info">
-        <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaa aaaaaaaaaa aaaaaaaaaaa aaaaaaaaaaa aaaaaaaaaa aaaaaaaaaaaa aaaaa aaaaa a a a a aaaaa aaa aaa aaaaa aaa aaa aaa aaaaa aaa aa aaaaaa aa</p>
+      <div className="info reverse">
+        <p>Dans ce court métrage, on suit l'histoire d'un personnage lambda, et sa chute sans fin vers une folie qui prendra le dessus de son esprit, en mettant l'accent sur la personnification de cette folie,</p>
         <img src={baba2} alt="" />
       </div>
     </div>
