e493a54b8bae349a63d563564c4042652edf2952
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-12-13 07:36:42 +0000

    passage to eslint error

--- src/compoments/moved/project/Dualitee/firstlayer/first_layer.jsx
+++ src/compoments/moved/project/Dualitee/firstlayer/first_layer.jsx
@@ -14,7 +14,7 @@ function first_layer_project_dualitee() {
         </div>
         <div className="desc">
           <img src={oscar} alt="" />
-          <p>Dans ce projet film, on s'embarque dans ce qui ressemble plus a une experimentation qu'a un vrai court métrage. Il a pour but de privélégier le métaphorique, au détriment de la compréhension générale de l'oeuvre</p>
+          <p>Dans ce projet film, on s&apos;embarque dans ce qui ressemble plus a une experimentation qu&apos;a un vrai court métrage. Il a pour but de privélégier le métaphorique, au détriment de la compréhension générale de l&apos;oeuvre</p>
         </div>
       </div>
     </div>
