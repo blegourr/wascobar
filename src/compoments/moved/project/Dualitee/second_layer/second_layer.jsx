1caf1fee86384892282fcccba8ae52a2f78a9b47
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-12-15 19:54:13 +0100

    correction to responsive and add page

--- src/compoments/moved/project/Dualitee/second_layer/second_layer.jsx
+++ src/compoments/moved/project/Dualitee/second_layer/second_layer.jsx
@@ -1,14 +1,11 @@
-import './second_layer.css'
-
 import oscar2 from './assets/oscar2.jpg'
-
 
 function second_layer_page_dualitee() {
   return (
-    <div className='second_layer_page_dualitee'>
+    <div className='second_layer_projet_Film'>
       <div className="info">
-        <p>Dans ce court métrage, on suit l&apos;histoire d&apos;un personnage lambda, et sa chute sans fin vers une folie qui prendra le dessus de son esprit, en mettant l&apos;accent sur la personnification de cette folie.</p>
         <img src={oscar2} alt="" />
+        <p>Dans ce court métrage, on suit l&apos;histoire d&apos;un personnage lambda, et sa chute sans fin vers une folie qui prendra le dessus de son esprit, en mettant l&apos;accent sur la personnification de cette folie.</p>
       </div>
       <div className="info">
         <img src={oscar2} alt="" />
--- src/compoments/moved/project/anglais/first_layer/first_layer.jsx
+++ src/compoments/moved/project/anglais/first_layer/first_layer.jsx
@@ -7,7 +7,7 @@ import baba from './assets/baptiste.jpg.jpg'
 
 function first_layer_project_anglais() {
   return (
-    <div className='first_layer_project_anglais'>
+    <div className='first_layer_project_Film'>
       <div className='container'>
         <div className="titre">
           <h1>Proposé par Baptiste</h1>
