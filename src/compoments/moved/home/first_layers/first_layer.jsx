b76b8cc7fe5e36bfb280ee8b37edbe5992b7eec8
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-12-17 15:08:14 +0100

    first test to video in first_layer home

--- src/compoments/moved/home/first_layers/first_layer.jsx
+++ src/compoments/moved/home/first_layers/first_layer.jsx
@@ -1,26 +1,68 @@
+import { useEffect, useRef } from 'react';
+
 // import css
 import './firstLayer.css'
 
-import './assets/V1.mp4'
+// import assets
+import video from './assets/V1.mp4'
+import logo from '../../../fixed/header/assets/logo.jpg'
 
-function first_layer_Home() {
+
+function First_layer_Home() {
+
+  const videoRef = useRef(null);
+
+  
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    const handleCanPlayThrough = () => {
+      // La vidéo est prête à être lue, définir autoplay sur true
+      videoElement.autoplay = true;
+      videoElement.removeEventListener('canplaythrough', handleCanPlayThrough);
+    };
+
+    videoElement.addEventListener('canplaythrough', handleCanPlayThrough);
+
+    // Vérifier si la vidéo est prête à jouer
+    const canPlayPromise = videoElement.play();
+
+    if (canPlayPromise !== undefined) {
+      canPlayPromise
+        .then(() => {
+          // La vidéo peut être lue automatiquement
+        })
+        .catch((error) => {
+          // La lecture automatique n'est pas possible, l'utilisateur doit lancer la vidéo manuellement
+          console.error('La lecture automatique n\'est pas possible. Veuillez lancer la vidéo manuellement.', error);
+        });
+    }
+
+    return () => {
+      videoElement.removeEventListener('canplaythrough', handleCanPlayThrough);
+    };
+  }, []);
+
   return (
     <div className='first_layer_Home'>
       <div className="content">
         <div className="contentImg">
-          <img src="" alt="" />
+          <img src={logo} alt="" />
         </div>
         <div className="contentText">
-          
+          <h1>Wascobar</h1>
+          <p>Wascobar est un jeunes studio de cinéma crée par de jeunes étudiants en Bretagne</p>
         </div>
       </div>
       <div className="background">
-        <img src="" alt="" />
-        <video src=""></video>
+        {/* <img src="" alt="" /> */}
+        <div className="cache"></div>
+        <video src={video} autoPlay loop ref={videoRef}></video>
       </div>
     </div>
   );
 }
 
 
-export default first_layer_Home;
+export default First_layer_Home;
