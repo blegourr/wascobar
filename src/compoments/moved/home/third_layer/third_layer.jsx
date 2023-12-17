b969eff3b60925a54783adc2627d0aa16511dd05
Khalesis <126458936+Khalesis@users.noreply.github.com>
2023-12-17 19:51:55 +0000

    V1 textes page home

--- src/compoments/moved/home/third_layer/third_layer.jsx
+++ src/compoments/moved/home/third_layer/third_layer.jsx
@@ -7,19 +7,19 @@ function home_third_layer() {
     <div className='home_third_layer'>
       <h1>Notre équipe</h1>
       <div className="groups revers">
-        <p>hjehjhfi hfddddddd dddddd dddddd dddddddddd ddddddddd ddddddd dddddd dddddd dddd dddddddddd ddddddddddddddddddd ddddddddddddd ddddddd ddddddddddddd ddddddd dddddd dddddddd ddddddghjygdjgdyug</p>
+        <p>Wyatt est le moteur de ce projet, actuellement étudiant en audiovisuel, il a choisit de s'éxercer dans le dur pour faire ce qu'il aime avec ses potes.</p>
         <img src={Anglais} alt="" />
       </div>
       <div className="groups">
-        <p>hjehjhfi hfddddddd dddddd dddddd dddddddddd ddddddddd ddddddd dddddd dddddd dddd dddddddddd ddddddddddddddddddd ddddddddddddd ddddddd ddddddddddddd ddddddd dddddd dddddddd ddddddghjygdjgdyug</p>
+        <p>Baptiste c'est le deuxième moteur du projet. C'est celui qui a écrit en entier son film d'action de A à Z avec l'aide de Wyatt, et aussi le créateur de ce site internet </p>
         <img src={Anglais} alt="" />
       </div>
       <div className="groups revers">
-        <p>hjehjhfi hfddddddd dddddd dddddd dddddddddd ddddddddd ddddddd dddddd dddddd dddd dddddddddd ddddddddddddddddddd ddddddddddddd ddddddd ddddddddddddd ddddddd dddddd dddddddd ddddddghjygdjgdyug</p>
+        <p>Oscar c'est le compositeur de musique sur tout les projets, il est aussi gros fan de métal et adore créer de la musique. C'est ausi le créateur du projet Dualitée !</p>
         <img src={Anglais} alt="" />
       </div>
       <div className="groups">
-        <p>hjehjhfi hfddddddd dddddd dddddd dddddddddd ddddddddd ddddddd dddddd dddddd dddd dddddddddd ddddddddddddddddddd ddddddddddddd ddddddd ddddddddddddd ddddddd dddddd dddddddd ddddddghjygdjgdyug</p>
+        <p>Maxence aussi appelé plus communément Mayo, c'est celui qui prendra en image tout nos cours métrage et faire briller l'image comme il sait bien le faire.</p>
         <img src={MayoCherie} alt="" />
       </div>
 
