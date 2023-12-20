36a659e618c2d52c1d645832c35d32decdda52c1
Math-222 <145916791+Math-222@users.noreply.github.com>
2023-12-20 08:45:54 +0000

    résolution_problème_width

--- src/compoments/moved/Contact/First_layer_contact.jsx
+++ src/compoments/moved/Contact/First_layer_contact.jsx
@@ -6,21 +6,20 @@ import Twitch from './assets/Twitch.svg'
 function first_layer_contact() {
   return (
     <div className="First_layer_contact">
-      <h1>Contactez nous</h1>
-      <div className="groups first">
-        <img src={Youtube} alt="" />
-        <p>fuffize zdzdzdzdzdzdz dzdzdzdhf</p>
+      <div classeName="groups first">
+        <h2>Bienvenue dans cette magnifique page contact où vous pourrez nous contacter à travers ces différentes applications</h2>
+        <h1>Contactez nous</h1>
       </div>
       <div className="groups second">
         <div className="part one">
           <img src={Twitch} alt="" />
-          <p>hudzih zdzdzdzdzd dzdsqqsvsdqvqsqsqsqsqsqs qsqsqsqsqsqsqsqs qsqsqsqsqsqsqsqsdzdzdzdzdzdz dzddz</p>
+          <p>hudzih zsqsqsqs qsqsqsqsqsqsqsqs qsqsqsq sqsqsqsq sdzdzdzd zdzdz dzddz</p>
         </div> 
         <div className="part two">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
               </svg>
-              <p>duzss sssssss sssgfhdhgfdhg dfffffffffff ffffff idzbni</p>
+              <p>duzssghdjksnlgcqcgqggcgdrgr sssssss sssgfhdhgfdhg dfffffffffff ffffff idzbni</p>
         </div>
       </div>      
     </div>
