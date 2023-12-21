e646e2cdbd79b629fd07fce4c864908cfc315d3a
Math-222 <145916791+Math-222@users.noreply.github.com>
2023-12-21 16:15:09 +0000

    page_contact_vraiment_fini

--- src/compoments/moved/Contact/First_layer_contact.jsx
+++ src/compoments/moved/Contact/First_layer_contact.jsx
@@ -1,6 +1,5 @@
 import './First_layer_contact.css';
-
-import Youtube from './assets/Youtube.svg'
+import TrayNotif from './assets/trayNotif.jsx'
 import Twitch from './assets/Twitch.svg'
 
 function first_layer_contact() {
@@ -19,7 +18,11 @@ function first_layer_contact() {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
               </svg>
-              <p>Il y a également une boîte mail mit à votre disposition pour nous communiquer vos idées les plus farfelues et ainsi nous aider dans nos futurs films, toute aide est la bienvenue (Cadreurs, monteurs, figurant...)</p>
+              <p>Il y a également une boîte mail mit à votre disposition pour nous parler de vos idées les plus farfelues et nous aider dans nos futurs films, toute aide est la bienvenue (Cadreurs, monteurs, figurant...)</p>
+        </div>
+        <div className="part three">
+          <TrayNotif/>
+          <p>Nous avons aussi un serveur discord que vous pouvez rejoindre tout autant que vous êtes, pour nous contacter par message ou nous appeler sur notre serveur. Que ce soit plus ou moins important nous serons à votre écoute </p>
         </div>
       </div>      
     </div>
