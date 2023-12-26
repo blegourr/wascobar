dc8e77cf3c7ba5397946dbfb93bbefee0ebb50f8
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-12-26 20:59:56 +0100

    change opacity

--- src/page/Project/Dualite.jsx
+++ src/page/Project/Dualite.jsx
@@ -4,23 +4,29 @@ import HeaderCompongnement from '../../compoments/fixed/header/header';
 import FirstLayer from '../../compoments/moved/project/Dualitee/firstlayer/first_layer'
 import SecondLayer from '../../compoments/moved/project/Dualitee/second_layer/second_layer';
 import FooterCompongnement from '../../compoments/fixed/footer/Footer'
-function PageDualite({data, setData, setFoundName}) {
-    return (
-      <div className='PageDualite'>
-        <HeaderCompongnement data={data} setData={setData} setFoundName={setFoundName}/>
-        <FirstLayer />
-        <SecondLayer />
-        <FooterCompongnement/>
 
+import '../background/Dualite/Dualite.css'
+import back from '../background/Dualite/assets/back.jpg'
+
+function PageDualite({ data, setData, setFoundName }) {
+  return (
+    <div className='PageDualite'>
+      <HeaderCompongnement data={data} setData={setData} setFoundName={setFoundName} />
+      <FirstLayer />
+      <SecondLayer />
+      <FooterCompongnement />
+      <div className="backgroundFixedPageDualite">
+        <img src={back} alt="" />
       </div>
+    </div>
     );
-  }
-  
+}
 
-  PageDualite.propTypes = {
-    data: PropTypes.object.isRequired,
-    setData : PropTypes.func.isRequired,
-    setFoundName : PropTypes.func.isRequired,
-  }
-  
-  export default PageDualite;
+
+PageDualite.propTypes = {
+  data: PropTypes.object.isRequired,
+  setData: PropTypes.func.isRequired,
+  setFoundName: PropTypes.func.isRequired,
+}
+
+export default PageDualite;
