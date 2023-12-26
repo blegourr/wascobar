7f26a2e2541f63de77c24cc161900e2a94b74302
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-12-26 19:43:33 +0100

    change img

--- src/compoments/moved/project/Romance/second_layer/second_layer.jsx
+++ src/compoments/moved/project/Romance/second_layer/second_layer.jsx
@@ -1,16 +1,16 @@
 import img1 from './Assets/img3.jpg'
-import img2 from './Assets/img4.avif'
+import img2 from './Assets/img4.jpg'
 
 function second_layer() {
   return (
     <div className='second_layer_projet_Film'>
       <div className="info">
         <img src={img1} alt="" />
-        <p>C'est un projet pour l'instant en état d'embryon. On compte faire une romance humouristique .</p>
+        <p>C&apos;est un projet pour l&apos;instant en état d&apos;embryon. On compte faire une romance humouristique .</p>
       </div>
       <div className="info reverse">
         <img src={img2} alt="" />
-        <p>Vous l'aurez remarqué avec ces textes trés court, on ne sait encore que peu de chose sur ce qu'on va pouvoir faire avec ce film</p>
+        <p>Vous l&apos;aurez remarqué avec ces textes trés court, on ne sait encore que peu de chose sur ce qu&apos;on va pouvoir faire avec ce film</p>
       </div>
     </div>
   );
