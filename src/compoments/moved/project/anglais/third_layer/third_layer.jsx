b6acd92bcb01092a3d74ad38c05d74d05c742d13
Blegourr <91756178+blegourr@users.noreply.github.com>
2024-01-05 12:04:04 +0100

    update text

--- src/compoments/moved/project/anglais/third_layer/third_layer.jsx
+++ src/compoments/moved/project/anglais/third_layer/third_layer.jsx
@@ -14,7 +14,7 @@ function third_layer() {
         Si vous êtes passionné par l&apos;art du cinéma, nous vous invitons à <a href="https://forms.gle/h9ApY3nV1oowVV7r6" target="_blank" rel="noopener noreferrer">
           participer à cette aventure
         </a>{''}
-        . Que vous soyez acteur, technicien, costumier, maquilleur, ou simplement passionné par le cinéma, votre contribution serait inestimable. Nous tenons à préciser que ce projet est à but non lucratif, et toute participation se fera bénévolement. Le tournage aura lieu cet été en Bretagne, principalement dans le Morbihan.
+        . Que vous soyez acteur, technicien, costumier, maquilleur, ou simplement passionné par le cinéma, votre contribution serait inestimable. Nous tenons à préciser que ce projet est à but non lucratif, et toute participation se fera bénévolement. Le tournage aura lieu cet été en Bretagne, principalement dans le Morbihan. <a href="https://docs.google.com/document/d/1V9t7lG_xOXxPR6hXiFYND5tGpIELCbhV9DBVs0Gex08/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Pour plus d&apos;information cliquer ici !</a>{''}
       </p>
     </div>
   );
